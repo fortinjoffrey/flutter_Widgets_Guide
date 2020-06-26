5f2206a70a02e7247ac148c5f20340e582ef55c3
Joffrey Fortin <fortinjoffrey@gmail.com>
2020-06-26 14:43:56 +0200

    Upload demo.gif

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -4,7 +4,7 @@ import 'package:widgets_guide/models/routes_name.dart';
 
 const List<Choice> mainChoices = [
   Choice('List Views', RoutesName.listviews),
-  Choice('SliverList TODO', RoutesName.sliverlists),
+  Choice('SliverList (coming next)', RoutesName.sliverlists),
 ];
 
 const List<Choice> listviewChoices = [
